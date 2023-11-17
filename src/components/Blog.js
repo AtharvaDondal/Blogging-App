e0c59c472e722dc4f94cd7e22f610bc80911e42e
AtharvaDondal <120319760+AtharvaDondal@users.noreply.github.com>
2023-11-17 16:44:46 +0530

    fixed issue that title is not adding, and optimize code

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,16 +2,19 @@ import { useState } from "react";
 
 //Blogging App using Hooks
 export default function Blog() {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+  // const [title, setTitle] = useState("");
+  // const [content, setContent] = useState("");
+  const [formData, setFormData] = useState({
+    title: "",
+    content: ""
+  })
   const [blogs, setBlogs] = useState([]);
 
   //Passing the synthetic event as argument to stop refreshing the page on submit
   function handleSubmit(e) {
     e.preventDefault();
-    setBlogs([{ title, content }, ...blogs]);
-    setTitle("")
-    setContent("");
+    setBlogs([{title: formData.title, content:formData.content}, ...blogs]);
+     setFormData({title:"",content:""})
     console.log(blogs);
   }
 
@@ -29,8 +32,8 @@ export default function Blog() {
             <input
               className="input"
               placeholder="Enter the Title of the Blog here.."
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
+              value={formData.title}
+              onChange={(e) => setFormData({title: e.target.value,content:formData.content})}
             />
           </Row>
 
@@ -39,8 +42,8 @@ export default function Blog() {
             <textarea
               className="input content"
               placeholder="Content of the Blog goes here.."
-              value={content}
-              onChange={(e) => setContent(e.target.value)}
+              value={formData.content}
+              onChange={(e) => setFormData({title:formData.title,content: e.target.value})}
             />
           </Row>
 
