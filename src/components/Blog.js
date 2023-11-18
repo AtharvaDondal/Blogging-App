7535ccda0a5a7d799055f0d1ce391e144c607273
AtharvaDondal <120319760+AtharvaDondal@users.noreply.github.com>
2023-11-18 09:14:26 +0530

    added deleting blogs functionality

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -6,16 +6,20 @@ export default function Blog() {
   // const [content, setContent] = useState("");
   const [formData, setFormData] = useState({
     title: "",
-    content: ""
-  })
+    content: "",
+  });
   const [blogs, setBlogs] = useState([]);
 
   //Passing the synthetic event as argument to stop refreshing the page on submit
   function handleSubmit(e) {
     e.preventDefault();
-    setBlogs([{title: formData.title, content:formData.content}, ...blogs]);
-     setFormData({title:"",content:""})
+    setBlogs([{ title: formData.title, content: formData.content }, ...blogs]);
+    setFormData({ title: "", content: "" });
     console.log(blogs);
+  }
+
+  function removeBlog(i){
+    setBlogs(blogs.filter((blog,index)=>i !== index))
   }
 
   return (
@@ -33,7 +37,12 @@ export default function Blog() {
               className="input"
               placeholder="Enter the Title of the Blog here.."
               value={formData.title}
-              onChange={(e) => setFormData({title: e.target.value,content:formData.content})}
+              onChange={(e) =>
+                setFormData({
+                  title: e.target.value,
+                  content: formData.content,
+                })
+              }
             />
           </Row>
 
@@ -43,7 +52,9 @@ export default function Blog() {
               className="input content"
               placeholder="Content of the Blog goes here.."
               value={formData.content}
-              onChange={(e) => setFormData({title:formData.title,content: e.target.value})}
+              onChange={(e) =>
+                setFormData({ title: formData.title, content: e.target.value })
+              }
             />
           </Row>
 
@@ -60,6 +71,10 @@ export default function Blog() {
         <div className="blog" key={index}>
           <h3>{blog.title}</h3>
           <p>{blog.content}</p>
+
+          <div className="blog-btn">
+            <button onClick = {()=>removeBlog(index)}className="btn remove">Delete</button>
+          </div>
         </div>
       ))}
     </>
